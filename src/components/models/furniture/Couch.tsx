650fa6a85ca15408c5c4ab795b524cfaea6d8d3c
Nithya Reddy <62687733+nithya-reddych@users.noreply.github.com>
2025-01-27 14:10:28 -0500

    Update Couch.tsx

--- src/components/models/furniture/Couch.tsx
+++ src/components/models/furniture/Couch.tsx
@@ -6,7 +6,7 @@ export function Couch() {
 
   return (
     <group>
-      <group position={[0, 0, 0.5]} rotation={[0, Math.PI, 0]}>
+      <group position={[0, 0.1, 0.5]} rotation={[0, Math.PI, 0]}>
         <primitive object={sofa.scene} scale={[0.04, 0.03, 0.03]} />
       </group>
 
